f7f065f4ec30d1f4eb755c2f9715a7ee1a678b3e
Antonio Antillon <a.antillon@codigo-abierto.net>
2011-07-26 19:03:59 -0700

    Modified Rails version to 3.1.0.rc4

--- lib/generators/best_in_place/setup_generator.rb
+++ lib/generators/best_in_place/setup_generator.rb
@@ -5,7 +5,8 @@ module BestInPlace
     desc "Copies best_in_place.js to the /public/javascripts folder of your app."
 
     def copy_js
-      copy_file "best_in_place.js", "public/javascripts/best_in_place.js"
+      #copy_file "best_in_place.js", "public/javascripts/best_in_place.js"
+      copy_file "best_in_place.js", "assets/javascripts/best_in_place.js"
     end
   end
 end
